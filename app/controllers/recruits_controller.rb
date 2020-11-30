8139738bd92528da5f83323a44797c4b6cb0e0e0
Jennifer Lee <jlee970124@gmail.com>
2020-11-30 14:50:56 +0900

    FEAT: recruit methods and athlete's show page finished

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,22 +1,34 @@
 class RecruitsController < ApplicationController
+  before_action :set_recruit, only: :destroy
+
   def index
+    @recruits = Recruits.all
   end
 
-  def show
-  end
-
-  def new
-  end
+  # def show; end
 
   def create
+    @athlete = Athlete.find(params[:athlete_id])
+    @recruit = Recruit.new
+    @recruit.athlete = @athlete
+
+    if @recruit.save
+      redirect_to recruits_path
+    else
+      render 'athletes/show'
+    end
   end
 
-  def destroy
-  end
+  def destroy; end
 
-  # def edit
-  # end
+  # def edit; end
 
   # def update
   # end
+
+  private
+
+  def set_recruit
+    @recruit = Recruit.find(params[:id])
+  end
 end
