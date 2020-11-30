60fcb99af318d4dd827f430d0ff52120d288844f
Victoria Draper <victoria.draper27@gmail.com>
2020-11-30 16:03:52 +0900

    FEAT: Added SearchBar HTML Partial

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,6 +1,10 @@
 class AthletesController < ApplicationController
   def index
-    @athletes = Athlete.all
+    if params[:query].present?
+      @athletes = Athlete.search(params[:query])
+    else
+      @athletes = Athlete.all
+    end
   end
 
   def show
--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,2 +1,16 @@
 class Athlete < ApplicationRecord
+  has_many :athlete_events
+  has_many :events, through: :athlete_events
+  has_many :recruits
+  has_many :coaches, through: :recruits
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [:first_name, :last_name, :grad_year, :rating, :team, :nationality],
+    associated_against: {
+      event: [:location, :date]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
