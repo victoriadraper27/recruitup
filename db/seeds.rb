7c4669a068f0158f9872e6c17c61e3b16e3130d8
Victoria Draper <victoria.draper27@gmail.com>
2020-11-30 14:20:08 +0900

    ARCH: Added Faker Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing the Database...."
+Organization.destroy_all
+Sport.destroy_all
+User.destroy_all
+Athlete.destroy_all
+
+
+puts "Generating Athletes"
+
+masters = Organization.create!(
+  name: 'Masters'
+  )
+
+golf = Sport.create!(
+  name: 'golf'
+  )
+
+user_attributes = {
+  email: 'coachie@email.com',
+  password: 'coachiecoach',
+  first_name: 'Butch',
+  last_name: 'Harmon',
+
+}
+user = User.new(user_attributes)
+user.organization = masters
+user.sport = golf
+user.save
+
+puts "Generated #{user.name} user"
+
+25.times do
+  athlete = Athlete.create!( first_name: Faker::Name.female_first_name,
+                  last_name: Faker::Name.last_name  ,
+                  grad_year: rand(2022..2025),
+                  team: Faker::Team.name,
+                  team_url: "www.#{Faker::Team.name}.com",
+                  nationality: Faker::Nation.nationality,
+                  rating: Faker::Number.decimal(l_digits: 3, r_digits: 3))
+  puts "Generated #{athlete.name}"
+end
+
+
+puts "Generated #{user.name} user"
+puts "Generated #{Athlete.count} athletes"
