7c323d9553756ca717582d3b207fcb4406555a3f
Victoria Draper <victoria.draper27@gmail.com>
2020-12-04 11:10:42 +0900

    FIX: Seeding Error & Models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,4 @@
 class Chatroom < ApplicationRecord
   has_many :messages
-  belongs_to :sport
-  belongs_to :organization
+  has_many :teams
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :organization
   belongs_to :sport
+  belongs_to :chatroom
   belongs_to :selected_schedule, class_name: 'Schedule', foreign_key: :selected_schedule_id, optional: true
 
   has_many :recruits, dependent: :destroy
