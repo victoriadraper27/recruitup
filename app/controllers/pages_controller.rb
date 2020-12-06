11a883ad6c695f6ffbb8e692a60bf7bc95469990
Victoria Draper <victoria.draper27@gmail.com>
2020-12-06 14:00:35 +0900

    FEAT: Added Upcoming Event Card to Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,13 +7,15 @@ class PagesController < ApplicationController
     @user = current_user
     @team = @user.team
     @schedules = @team.schedules
-    @schedule = @schedules.first
+    # @schedule = @schedules.first
     # @schedule = @team.selected_schedule
     @schedule_event = ScheduleEvent.new
     @team = current_user.team
-    # @schedule = @team.selected_schedule
+    @schedule = @team.selected_schedule || @schedules.first
     # @events = @team.selected_schedule.events
+    @event = @schedule.events.first
     @athletes = policy_scope(Athlete)
+    @athlete = (policy_scope(Athlete).where(!policy_scope(Recruit))).sample
     @recruits = @team.recruits.all
     @unavailable_day = UnavailableDay.new
     @unavailable_days = policy_scope(Event).order(created_at: :desc)
