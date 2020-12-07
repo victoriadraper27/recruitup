ddc995ef76b4cba4e07cbbfdfe4fd1c88e735834
Jennifer Lee <jlee970124@gmail.com>
2020-12-07 15:38:36 +0900

    FEAT: top navs added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :set_time_zone, if: :devise_controller?
+  before_action :set_time_zone, if: :user_signed_in?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
-  
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  # def set_time_zone
-  #   Time.zone = current_user.time_zone
-  # end
+  def set_time_zone
+    Time.zone = current_user.time_zone
+  end
 end
