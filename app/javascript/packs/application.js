b431def452f6da63991af3b5b358ea8999216c55
Jennifer Lee <jlee970124@gmail.com>
2020-12-08 19:45:38 +0900

    FIX: table sorting does not work yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('datatables.net-bs4')
+require("datatables.net-bs4")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -27,7 +27,6 @@ import "bootstrap";
 import $ from 'jquery';
 global.$ = jQuery;
 
-// import "../plugins/init_flatpickr";
 
 // Internal imports, e.g:
 import { initSelect2 } from '../plugins/init_select2';
@@ -47,22 +46,23 @@ document.addEventListener('turbolinks:load', () => {
 
   initMapbox();
 
-    $('#dashboard_athlete').DataTable( {
-        "scrollY":        "200px",
-        "scrollCollapse": true,
-        "paging":         false
+  $('#dashboard_athlete').DataTable( {
+    "scrollY":        "200px",
+    "scrollCollapse": true,
+    "paging":         false
   });
-  $('#dashboard_recruit').DataTable( {
-        "scrollY":        "200px",
-        "scrollCollapse": true,
-        "paging":         false
-  });
-  $('#recruit-index').DataTable( {
-        "scrollY":        "200px",
-        "scrollCollapse": true,
-        "paging":         false
-  });
-  $('#athlete-index').DataTable();
+
+  // $('#dashboard_recruit').DataTable( {
+  //       "scrollY":        "200px",
+  //       "scrollCollapse": true,
+  //       "paging":         false
+  // });
+  // $('#recruit-index').DataTable( {
+  //       "scrollY":        "200px",
+  //       "scrollCollapse": true,
+  //       "paging":         false
+  // });
+  // $('#athlete-index').DataTable();
 
 //   $('.dropdown-menu').on('hidden.bs.dropdown', function (event) {
 //     console.log(event)
